bf2c183ef8dc5f742678ac1c772399c359a17a72
Jarilton <jariltonpereira02@hotmail.com>
2022-07-29 22:57:52 -0300

    Testando as saidas do componente

--- src/tests/unit/Lance.spec.js
+++ src/tests/unit/Lance.spec.js
@@ -5,5 +5,18 @@ test('Não aceita lance com valor menor do que zero', () => {
       const wrapper = mount(Lance)
       const input = wrapper.find("input")
       input.setValue(-100)
-      expect(wrapper).toBeTruthy()
+      const lancesEmitidos = wrapper.emitted("novo-lance")
+      wrapper.trigger("submit")
+      expect(lancesEmitidos).toBeUndefined()
+})
+
+test('Emite um lance quando o valor for maior do que zero', () => {
+      const wrapper = mount(Lance)
+      const input = wrapper.find("input")
+      input.setValue(100)
+      wrapper.trigger("submit")
+      const lancesEmitidos = wrapper.emitted("novo-lance")
+      expect(lancesEmitidos).toHaveLength(1)
+
+      console.log("lancesEmitidos",lancesEmitidos)
 })
