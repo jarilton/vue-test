f56cafdd465cc572600a3332d1cb499921cc8fa1
Jarilton <jariltonpereira02@hotmail.com>
2022-07-30 16:28:07 -0300

    Adicionando props nos testes

--- src/tests/unit/Lance.spec.js
+++ src/tests/unit/Lance.spec.js
@@ -1,4 +1,4 @@
-import Lance from "@/components/Lance" 
+import Lance from "@/components/Lance"
 import { mount } from "@vue/test-utils"
 
 test('Não aceita lance com valor menor do que zero', () => {
@@ -28,5 +28,34 @@ test('Emite o valor esperado de um lance válido', () => {
 
       const lances = parseInt(lancesEmitidos, [0][0])
       expect(lances).toBe(100)
+})
 
+describe("Um lance com valor minimo", () => {
+      test("Todos os lances devem possuir um valor maior que o valor minimo informado", () => {
+            const wrapper = mount(Lance, {
+                  propsData: {
+                        lanceMinimo: 300,
+                  }
+            })
+            const input = wrapper.find("input")
+            input.setValue(400)
+            wrapper.trigger("submit")
+            const lancesEmitidos = wrapper.emitted("novo-lance")
+            expect(lancesEmitidos).toHaveLength(1)
+      })
+
+      test("Emite o valor esperado de um lance válido", () => {
+            const wrapper = mount(Lance, {
+                  propsData: {
+                        lanceMinimo: 300,
+                  }
+            })
+            const input = wrapper.find("input")
+            input.setValue(400)
+            wrapper.trigger("submit")
+            const lancesEmitidos = wrapper.emitted("novo-lance")
+            
+            const valorDoLance  = parseInt(lancesEmitidos[0][0])
+            expect(valorDoLance).toBe(400)
+      })
 })
