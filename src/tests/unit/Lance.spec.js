3e51ee5d639fff1cd588c013e9f17eb64d843efa
Jarilton <jariltonpereira02@hotmail.com>
2022-07-29 22:38:53 -0300

    Encontrando elemento input

--- src/tests/unit/Lance.spec.js
+++ src/tests/unit/Lance.spec.js
@@ -3,5 +3,7 @@ import { mount } from "@vue/test-utils"
 
 test('Não aceita lance com valor menor do que zero', () => {
       const wrapper = mount(Lance)
+      const input = wrapper.find("input")
+      input.setValue(-100)
       expect(wrapper).toBeTruthy()
 })
