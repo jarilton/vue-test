32414fe8fae6399d27cca0a5366553faca7c6bb1
Jarilton <jariltonpereira02@hotmail.com>
2022-07-30 20:00:43 -0300

    Teste componente avaliador simulando RouterLink

---
+++ src/tests/unit/Avaliador.spec.js
@@ -0,0 +1,35 @@
+import Avaliador from '@/views/Avaliador'
+import { mount, RouterLinkStub } from '@vue/test-utils'
+import { getLeiloes } from '@/http'
+import flushPromises from 'flush-promises'
+
+jest.mock("@/http")
+
+const leiloes = [
+      {
+            "produto": "Livro da Casa do Código",
+            "descricao": "Um livro super completo, sobre um assunto incrível.",
+            "lanceInicial": 500
+      },
+      {
+            "produto": "Ebook da Casa do Código",
+            "descricao": "Um livro com um conteúdo muito interessante sobre VueJS",
+            "lanceInicial": "500",
+      },
+]
+
+describe("Um avaliador que se conecta com a API", () => {
+      test("Garantir que mostra todos os leilões retornados pela API", async () => {
+            getLeiloes.mockResolvedValueOnce(leiloes)
+
+            const wrapper = mount(Avaliador, {
+                  stubs: {
+                        RouterLink: RouterLinkStub
+                  }
+            })
+            await flushPromises()
+            const totalLeiloesExibidos = wrapper.findAll(".leilao").length
+
+            expect(totalLeiloesExibidos).toBe(leiloes.length)
+      })
+})
